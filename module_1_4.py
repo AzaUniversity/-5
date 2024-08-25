6d0be0e295df92754b2f4f11bdb5288c64c5102d
AzaUniversity <azamasha0410@gmail.com>
2024-08-25 12:15:39 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+#my_string = input('Как вас зовут?: ')
+# print(len(my_string))
+
+print('my_string'.upper()) # в вверхнем регистре
+print('MY_STRING'.lower()) # в нижнем регистре
+print('my_str    ing'.replace(' ' , '')) # без пробелов
+print('my_string'[0]) # первый символ строки
+print('my_sting'[-1]) # последний символ строки
